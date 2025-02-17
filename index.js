232e6fe5f79120d5a50770261369b6e887f98b0c
Uche Godswill <auchjee@gmail.com>
2025-02-17 23:01:42 +0100

    Update algorithm

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function calculateRollover(){
     let odds = document.getElementById("odds").value;
     let rollover = document.getElementById("rollover").value;
 
-    let returns = (stake * odds) ^ rollover;
+    let stakeOdds = stake * odds;
+    let returns = Math.pow(stakeOdds, rollover);
     document.getElementById("returns").innerHTML = `Wow!, after the gamble, you'll get ₦${returns} in return.`
 }
