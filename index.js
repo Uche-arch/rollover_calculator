274df34bab9a04cef39ffa41145ed1c10e48856c
Uche Godswill <auchjee@gmail.com>
2025-02-18 03:06:35 +0100

    Update code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-
 function calculateRollover(){
     let stake = parseFloat(document.getElementById("stake").value);
     let odds = parseFloat(document.getElementById("odds").value);
     let rollover = parseFloat(document.getElementById("rollover").value);
 
-    let stakeOdds = stake * odds;
-    let returns = stakeOdds * (rollover + 1);
-    document.getElementById("returns").innerHTML = `Wow!, after the gamble, you'll get ₦${returns.toLocaleString()} in return.`;
-}
+    let returns = stake;
+    for(let i = 1; i <= rollover; i++){
+        returns *= odds;
+    }
+    document.getElementById("returns").innerHTML = `Wow! Total amount after ${rollover} rollovers is ₦${returns.toLocaleString()}`;
+};
